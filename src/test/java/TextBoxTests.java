aca05187dea185258c1f1e5963a2250712a92793
annashv <annashivrina@gmail.com>
2024-03-07 19:24:34 +0100

    First test

---
+++ src/test/java/TextBoxTests.java
@@ -0,0 +1,31 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTests {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+
+    @Test
+    void fillFormTest() {
+        open("/text-box");
+        if ($(".fc-button-label").isDisplayed())
+            $(".fc-button-label").click();
+
+        $("#userName").setValue("Alex Egorov");
+        $("#userEmail").setValue("alex@egorov.com");
+        $("#currentAddress").setValue("Some address 1");
+        $("#permanentAddress").setValue("Another address 1");
+        $("#submit").scrollTo().click();
+
+        $("#output").shouldHave(text("Alex Egorov"), text("alex@egorov.com"),
+                text("Some address 1"), text("Another address 1"));
+    }
+}
